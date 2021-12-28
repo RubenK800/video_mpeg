6b627d08b5fd8543c55531bab7baba03965ec81b
Ruben Kubalyan <kubalyan-ruben@mail.ru>
2021-12-28 10:56:04 +0400

    front Login, external css files optimal import, external images import (+ bug fixed), make  username unique with laravel migration, change vue component elements addresses to dynamically with bindings

--- video_mpeg_backend/routes/web.php
+++ video_mpeg_backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SimpleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group([
+    'middleware' => ['web', 'cors'],
+//    'namespace' => $this->namespace,
+//    'prefix' => 'api',
+], function ($router) {
+    //Add you routes here, for example:
+    Route::get('/', [SimpleController::class, 'index']);
 });
+
+
